8891511e77e6399eedbe488f78c353261302967c
Katie <Tatsu@Katies-MacBook.nyc.rr.com>
2019-09-07 21:09:53 -0400

    created game loic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else alert("Sorry, try again");
+
+} 
